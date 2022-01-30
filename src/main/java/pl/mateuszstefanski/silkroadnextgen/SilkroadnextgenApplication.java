676b3e92e6b2627ef1d90aa07d874abc3911c515
84MateuszStefanski <84mateuszstefanski@gmail.com>
2022-01-30 21:42:30 +0100

    Architecture

---
+++ src/main/java/pl/mateuszstefanski/silkroadnextgen/SilkroadnextgenApplication.java
@@ -0,0 +1,13 @@
+package pl.mateuszstefanski.silkroadnextgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkroadnextgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilkroadnextgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mateuszstefanski/silkroadnextgen/SilkroadnextgenApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateuszstefanski.silkroadnextgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilkroadnextgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
